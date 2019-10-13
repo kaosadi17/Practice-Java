f489ef46d5a14b5099db8d4b3766d03e50fed67a
Amali Osadi <kaosadi17@gmail.com>
2019-10-13 13:45:14 +0530

    Find ASCII value of given character

---
+++ Codes/FindAscii.java
@@ -0,0 +1,8 @@
+public class FindAscii{
+    public static void main(String args[]){
+        char ch = 'P';
+        int num =(int)ch;
+
+        System.out.println("ASCII value of Character "+ ch + " is " + num);
+    }
+}
